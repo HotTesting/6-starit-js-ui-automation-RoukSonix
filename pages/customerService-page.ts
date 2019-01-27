963a3f37232ba5f42c169f6a0e7fc08769ad5e30
Andrey Lazarev <a.lazarev@bpmonline.com>
2019-01-27 14:40:19 +0200

    #style fix

--- pages/customerService-page.ts
+++ pages/customerService-page.ts
@@ -2,12 +2,12 @@ import { BasePage } from "./base-page";
 
 export class CustomerServicePage extends BasePage {
 
-	public nameField() 			{return browser.$('form[name="contact_form"] [name="name"]')}
-	public emailField() 		{return browser.$('form[name="contact_form"] [name="email"]')}
-	public subjectField() 		{return browser.$('form[name="contact_form"] [name="subject"]')}
-	public messageBox() 		{return browser.$('form[name="contact_form"] [name="message"]')}
-	public sendButton() 		{return browser.$('.btn[name="send"]')}
-	public successMessage() 	{return browser.$('.alert-success')}
+	public nameField()			{return browser.$('form[name="contact_form"] [name="name"]')}
+	public emailField()			{return browser.$('form[name="contact_form"] [name="email"]')}
+	public subjectField()		{return browser.$('form[name="contact_form"] [name="subject"]')}
+	public messageBox()			{return browser.$('form[name="contact_form"] [name="message"]')}
+	public sendButton()			{return browser.$('.btn[name="send"]')}
+	public successMessage()		{return browser.$('.alert-success')}
 
 	public openPageUrl() {
 		browser.url('/customer-service-s-0')
